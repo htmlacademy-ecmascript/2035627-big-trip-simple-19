941b65384fdd9fa98be8086dac95e018844f380c
Marharyta Overednaya <pandoraaa274@gmail.com>
2022-12-22 09:36:03 +0200

    fix list presenter

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -24,23 +24,26 @@ export default class ListPresenter extends Presenter {
   createPointViewState(point) {
     const destination = this.destinationsModel.findById(point.destinationId);
     const offerGroup = this.offerGroupsModel.findById(point.type);
-    const offerViewStates = offerGroup.items;
 
-    const offerCurrent = [];
-    for (const id of point.offerIds) {
-      offerCurrent.push(offerViewStates.find(o => o.id === id));
-    }
+    const offerViewStates = offerGroup.items
+      .filter((offer) =>
+        point.offerIds.includes(offer.id)
+      )
+      .map((offer) => ({
+        title: offer.title,
+        price: formatNumber(offer.price)
+      }));
 
-    console.log(point, destination);
     return {
       date: formatDate(point.startDate),
       icon: pointIconMap[point.type],
       title: `${pointTitleMap[point.type]} ${destination.name}`,
       startTime: formatTime(point.startDate),
+      startDate: point.startDate,
       endTime: formatTime(point.endDate),
-      endDate: formatDate(point.endDate),
+      endDate: point.endDate,
       basePrice: formatNumber(point.basePrice),
-      offers: offerCurrent
+      offers: offerViewStates
     };
   }
 }
